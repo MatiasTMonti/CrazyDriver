b7e06eadd3d7201bfb3852c8195cae55c88c5c55
Matias <matiasmonti2011@hotmail.com>
2023-09-24 16:59:42 -0300

    Change Logic EndGameManager

--- Mobile Dev Tp1/Assets/Scripts/MenuScripts/EndGameManager.cs
+++ Mobile Dev Tp1/Assets/Scripts/MenuScripts/EndGameManager.cs
@@ -6,32 +6,26 @@ public class EndGameManager : MonoBehaviour
     [SerializeField] private TMPro.TextMeshProUGUI textWin1;
     [SerializeField] private TMPro.TextMeshProUGUI textWin2;
 
-    private TMPro.TextMeshProUGUI text;
+    [SerializeField] private TMPro.TextMeshProUGUI text;
     private void Start()
     {
-        text = GetComponent<TMPro.TextMeshProUGUI>();
         if (multiplayer.isMultiplayer)
         {
             if (DatosPartida.player1Points > DatosPartida.player2Points)
             {
                 text.text = "Congratulations\nPlayer 1 win with:\n$" + DatosPartida.player1Points;
-                if (textWin1.IsActive())
-                {
-                    textWin1.text = "Congratulations\nPlayer 1 win with:\n$" + DatosPartida.player1Points;
-                }
             }
             else if (DatosPartida.player1Points < DatosPartida.player2Points)
             {
                 text.text = "Congratulations\nPlayer 2 win with:\n$" + DatosPartida.player2Points;
-                if (textWin2.IsActive())
-                {
-                    textWin2.text = "Congratulations\nPlayer 2 win with:\n$" + DatosPartida.player2Points;
-                }
             }
             else
             {
                 text.text = "Congratulations\nBoth player win with:\n$" + DatosPartida.player1Points;
             }
+
+            textWin1.text = "\n$" + DatosPartida.player1Points;
+            textWin2.text = "\n$" + DatosPartida.player2Points;
         }
         else
         {
