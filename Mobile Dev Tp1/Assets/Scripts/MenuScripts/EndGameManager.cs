68ae7cd27d61c23a792b6869486b5cba5ae9ca81
Matias <matiasmonti2011@hotmail.com>
2023-09-24 13:43:25 -0300

    Change Script EndGame

--- Mobile Dev Tp1/Assets/Scripts/MenuScripts/EndGameManager.cs
+++ Mobile Dev Tp1/Assets/Scripts/MenuScripts/EndGameManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class EndGameManager : MonoBehaviour
 {
     [SerializeField] private MultiplayerScriptableObject multiplayer;
+    [SerializeField] private TMPro.TextMeshProUGUI textWin1;
+    [SerializeField] private TMPro.TextMeshProUGUI textWin2;
 
     private TMPro.TextMeshProUGUI text;
     private void Start()
@@ -13,10 +15,18 @@ public class EndGameManager : MonoBehaviour
             if (DatosPartida.player1Points > DatosPartida.player2Points)
             {
                 text.text = "Congratulations\nPlayer 1 win with:\n$" + DatosPartida.player1Points;
+                if (textWin1.IsActive())
+                {
+                    textWin1.text = "Congratulations\nPlayer 1 win with:\n$" + DatosPartida.player1Points;
+                }
             }
             else if (DatosPartida.player1Points < DatosPartida.player2Points)
             {
                 text.text = "Congratulations\nPlayer 2 win with:\n$" + DatosPartida.player2Points;
+                if (textWin2.IsActive())
+                {
+                    textWin2.text = "Congratulations\nPlayer 2 win with:\n$" + DatosPartida.player2Points;
+                }
             }
             else
             {
